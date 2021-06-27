b9ecb7f78e7ae4d702e4b8997869a8edb2793ea3
Katie Volz <iggyvolz@gmail.com>
2021-06-27 16:26:32 -0400

    Add import to test

--- test/TestTable.php
+++ test/TestTable.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace iggyvolz\phlum\test;
 
 use iggyvolz\phlum\MemoryDriver\AllIndex;
+use iggyvolz\phlum\MemoryDriver\SearchIndex;
+use iggyvolz\phlum\MemoryDriver\UniqueSearchIndex;
 use iggyvolz\phlum\PhlumTable;
 
 #[AllIndex]
